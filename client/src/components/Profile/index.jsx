19fd02be337ca7b1bfcfb41eaeccc78362a0cd66
יותם הרמס <yotamhermes@gmail.com>
2022-04-25 12:16:50 +0300

    tags to profile

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,12 +1,21 @@
 import "./index.css";
 import experiencesMock from "../../assets/mocks/EXPERIENCE.json";
+import PEOPLE_TAGS from "../../assets/mocks/PEOPLE_TAGS.json";
+import TAGS from "../../assets/mocks/TAGS.json";
 import Timeline from "./Timeline";
+import { Chip } from "@mui/material";
 
 const Profile = ({ link, name, experience = experiencesMock }) => {
   link =
     "https://media-exp1.licdn.com/dms/image/C4D03AQHwdI64crX-6Q/profile-displayphoto-shrink_400_400/0/1606576176064?e=1656547200&v=beta&t=XvRLVfxnaoLrzV08QWSUwQjyBOU3QeK5J8okKcOOkG8";
   name = "ofie elart";
   const currentRole = experience.filter((x) => x.endDate == null)[0];
+
+  const peopleTags = PEOPLE_TAGS.filter((pt) => pt.personId === 1);
+
+  const tagsInfo = peopleTags.map((pTag) =>
+    TAGS.find((t) => t.id === pTag.tagId)
+  );
 
   return (
     <div className="profile-container">
@@ -18,6 +27,11 @@ const Profile = ({ link, name, experience = experiencesMock }) => {
           {currentRole
             ? currentRole.role + " at " + currentRole.organization
             : "unemployed"}
+        </div>
+        <div className='profile-tags'>
+          {tagsInfo.map((tag) => (
+            <Chip label={tag.name} className='profile-tags-tag'></Chip>
+          ))}
         </div>
         <Timeline experienceArray={experiencesMock}></Timeline>
       </div>
