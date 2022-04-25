a2bed0293397ff47258ec2e2c5d97296c71b410a
ofirelarat <96ofir11@gmail.com>
2022-04-25 13:58:21 +0300

    colors

--- client/src/components/Profile/Skills/index.jsx
+++ client/src/components/Profile/Skills/index.jsx
@@ -1,12 +1,13 @@
 import ReactWordcloud from "react-wordcloud";
 
 export default ({ skills }) => {
-  return (
-    <ReactWordcloud
-      words={skills}
-      options={{ rotationAngles: [0, 0], rotations: 2, colors: [
-        
-      ] }}
-    />
-  );
+    return (
+        <ReactWordcloud
+            words={skills}
+            options={{
+                rotationAngles: [0, 0], rotations: 2, colors:
+                    ["#001219","#005F73", "#0A9396","#EE9B00","#CA6702","#BB3E03","#AE2012","#9B2226"]
+            }}
+        />
+    );
 };
