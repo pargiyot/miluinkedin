f6c9740bbe94d7c96a42a1e3873b0d2b926dfc27
ofirelarat <96ofir11@gmail.com>
2022-04-25 13:14:00 +0300

    .

--- client/src/components/Profile/Skills/index.jsx
+++ client/src/components/Profile/Skills/index.jsx
@@ -1,24 +1,6 @@
 import ReactWordcloud from 'react-wordcloud';
 
 export default ({skills}) => {
-    const words = [
-        {
-          text: 'told',
-          value: 64,
-        },
-        {
-          text: 'mistake',
-          value: 11,
-        },
-        {
-          text: 'thought',
-          value: 16,
-        },
-        {
-          text: 'bad',
-          value: 17,
-        },
-      ]
 
     return <ReactWordcloud 
                 words={skills}
