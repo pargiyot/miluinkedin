9b1f5fff0cebecece610f48476c2dc5c3985d0ac
ofirelarat <96ofir11@gmail.com>
2022-04-25 13:12:12 +0300

    fix skills styles

--- client/src/components/Profile/Skills/index.jsx
+++ client/src/components/Profile/Skills/index.jsx
@@ -20,5 +20,8 @@ export default ({skills}) => {
         },
       ]
 
-    return <ReactWordcloud words={skills} />
+    return <ReactWordcloud 
+                words={skills}
+                options={{rotationAngles: [0, 0], rotations: 2}}
+                size={[100, 100]} />
 }
