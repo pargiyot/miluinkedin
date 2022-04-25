78adec7c487afae59c5a345ad88b968f61516c51
ofirelarat <96ofir11@gmail.com>
2022-04-25 10:01:16 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Profile from './components/Profile';
-
+import Manage from './components/Manage';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" >
-          {/* <Route index element={<Search />} /> */}
+        <Route path="/" >
+          <Route index element={<Search />} />
           <Route path="/profile" element={<Profile />} />
-          {/* <Route path="search" element={<Search />} />
-          <Route path="manage" element={<Manage />} /> */}
+          <Route path="search" element={<Search />} />
+          <Route path="manage" element={<Manage />} />
         </Route>
       </Routes>
     </BrowserRouter>
