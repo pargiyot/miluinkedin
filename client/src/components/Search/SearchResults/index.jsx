a121ccdc277073a512fcc76b13f6e8a89746ddd4
ghershko3 <ghershko3@gmail.com>
2022-04-25 14:32:18 +0300

    NANI

--- client/src/components/Search/SearchResults/index.jsx
+++ client/src/components/Search/SearchResults/index.jsx
@@ -4,16 +4,23 @@ import PEOPLE_SKILLS from '../../../assets/mocks/PEOPLE_SKILLS.json'
 import PEOPLE_TAGS from '../../../assets/mocks/PEOPLE_TAGS.json'
 import TAGS from '../../../assets/mocks/TAGS.json'
 import SKILLS from '../../../assets/mocks/SKILLS.json'
+import { useNavigate  } from "react-router-dom"
 
 import { Avatar, Chip, Grid, List, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from '@mui/material'
 import _ from 'lodash'
 
 const SearchResults = ({ searchText }) => {
+    const navigate = useNavigate();
+
     const filteredPeopleByName = PEOPLE.filter(p => p.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    const handleClick = (e) => {
+        navigate(`/profile/${e.currentTarget.id}`)
+    }
 
 
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} style={{cursor: 'pointer' }}>
             {filteredPeopleByName.map(p => {
                 const peopleSkills = PEOPLE_SKILLS.filter(ps => ps.personId === p.id)
                 const peopleTags = PEOPLE_TAGS.filter(pt => pt.personId === p.id)
@@ -22,7 +29,7 @@ const SearchResults = ({ searchText }) => {
                 const tagsInfo = peopleTags.map(pTag => TAGS.find(tag => tag.id === pTag.tagId))
 
                 return (
-                    <ListItem alignItems="flex-start">
+                    <ListItem alignItems="flex-start" id={p.id} onClick={handleClick}>
                         <ListItemAvatar>
                             <Avatar alt={`${p.name}-img`} src={p.imgURL} />
                         </ListItemAvatar>
