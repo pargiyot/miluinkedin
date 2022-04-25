c9cb91d313c0842942ab2c3030d090352173e512
יותם הרמס <yotamhermes@gmail.com>
2022-04-25 10:28:09 +0300

    profile

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,5 +1,15 @@
-export default (props) => {
- return <div>
-     Profile!!!!
- </div>
-}
+import "./index.css";
+
+const Profile = ({ link }) => {
+  link =
+    "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
+
+  return (
+    <div className='profile-container'>
+      <div className="profile-background"></div>
+      <img className="profile-img" src={link}></img>
+    </div>
+  );
+};
+
+export default Profile;
