7037ca606083e5e0beb203aaadf513a99371688d
ghershko3 <ghershko3@gmail.com>
2022-04-25 18:33:49 +0300

    test add

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,23 +1,23 @@
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    gql
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
 } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: 'https://innocent-lemming-13.hasura.app/v1/graphql',
-    headers: {
-        "x-hasura-admin-secret": "zmqjascyz7YrkFric7zip7QHF0ZQaeODUkm6N1vpLi3ckPvKfuO64j3QOM0uhPbh"
-    },
-    cache: new InMemoryCache()
+  uri: 'https://innocent-lemming-13.hasura.app/v1/graphql',
+  headers: {
+    "x-hasura-admin-secret": "zmqjascyz7YrkFric7zip7QHF0ZQaeODUkm6N1vpLi3ckPvKfuO64j3QOM0uhPbh"
+  },
+  cache: new InMemoryCache()
 });
 
 
-export const getAllReservists = async() => {
-    const reservits = await client.query({
-        query: gql `
+export const getAllReservists = async () => {
+  const reservits = await client.query({
+    query: gql`
         query AllReservists {
             reservists {
             id
@@ -28,16 +28,52 @@ export const getAllReservists = async() => {
             }
         }      
       `
-    });
+  });
 
-    console.log(reservits)
-    return reservits;
+  console.log(reservits)
+  return reservits;
+}
+
+export const createReserver = async (imageUrl, linkedinName, linkedinURL, name, rank) => {
+  const reservitId = await client.mutate({
+    variables: {
+      imageUrl,
+      linkedinName,
+      linkedinURL,
+      name,
+      rank
+    },
+    mutation: gql`
+       mutation insert_reserva(
+         $imageUrl: String, 
+         $linkedinName: String, 
+         $linkedinURL: String, 
+         $name: String, 
+         $rank: String
+        ) {
+        insert_reservists(
+          objects: {
+            image_url: $imageUrl, 
+            linkedin_name: $linkedinName, 
+            linkedin_url: $linkedinURL,
+            name: $name, 
+            rank: $rank, 
+          }) {
+          returning {
+            id
+          }
+        }
+      }`
+  });
+
+  console.log(reservitId.data.insert_reservists.returning[0].id)
+  return reservitId.data.insert_reservists.returning[0].id;
 }
 
 //   "e317da3c-5387-4d4b-9cc8-d83c627abcbd"
-export const reservistById = async(id) => {
-    const reservist = await client.query({
-        query: gql `
+export const reservistById = async (id) => {
+  const reservist = await client.query({
+    query: gql`
             query myQuery($id: uuid!) {
               reservist: reservists_by_pk(id: $id) {
               image_url
@@ -62,10 +98,10 @@ export const reservistById = async(id) => {
             }
           }
         `,
-        variables: {
-            id
-        }
-    });
+    variables: {
+      id
+    }
+  });
 
-    return reservist.data.reservist;
+  return reservist.data.reservist;
 }
--- client/src/components/Manage/index.jsx
+++ client/src/components/Manage/index.jsx
@@ -1,5 +1,75 @@
-export default (props) => {
-    return <div>
-        Manage!!!
-    </div>
+import React from 'react'
+import { Grid, TextField, Typography, Button } from '@mui/material'
+import { createReserver } from '../../api';
+import { useFormik } from 'formik'
+
+const Manage = () => {
+    const formik = useFormik({
+        initialValues: {
+            imageUrl: '',
+            linkedinName: '',
+            linkedinURL: '',
+            name: '',
+            rank: '',
+        },
+        enableReinitialize: true,
+        onSubmit: async (values) => {
+            const { imageUrl, linkedinName, linkedinURL, name, rank } = values
+
+            const userId = await createReserver(imageUrl, linkedinName, linkedinURL, name, rank)
+            alert(userId)
+        },
+    });
+
+    return (
+        <Grid container direction={'column'} justifyContent="center" alignItems="center" spacing={3} style={{ backgroundColor: '#f3f2ee', height: '100%' }}>
+            <Grid item>
+                <Typography variant="h3">גייס מילואמניק</Typography>
+            </Grid>
+            <Grid item>
+                <Grid container direction={'column'} justifyContent="center" alignItems="center" spacing={1}>
+                    <Grid item>
+                        <TextField
+                            id="name"
+                            name="name"
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            label="שם"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="linkedinName"
+                            name="linkedinName"
+                            value={formik.values.linkedinName}
+                            onChange={formik.handleChange}
+                            label="שם בלינקדאין" />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="rank"
+                            name="rank"
+                            value={formik.values.rank}
+                            onChange={formik.handleChange}
+                            label="דרגה"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="imageUrl"
+                            name="imageUrl"
+                            value={formik.values.imageUrl}
+                            onChange={formik.handleChange}
+                            label="תמונה (לינק)"
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button onClick={formik.handleSubmit}>לזמן דחוף</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
 }
+
+export default Manage
