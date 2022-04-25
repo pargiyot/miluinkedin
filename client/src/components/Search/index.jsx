e2f0fd4b6467ce4a88dde314c6d0f23f30f03c09
ghershko3 <ghershko3@gmail.com>
2022-04-25 11:56:17 +0300

    list

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react"
-import './index.css'
+// import { useTheme } from '@mui/material/styles';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+import { useState } from "react";
+import './index.css';
+import SearchResults from './SearchResults'
+import { Grid } from '@mui/material'
 
-export default (props) => {
+export default () => {
     const [searchText, setSearchText] = useState('')
+
+    // const theme = useTheme();
+    // const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
         <div className="container">
-            <input
-                className="search-input"
-                placeholder="חפש מילואימניק"
-                value={searchText}
-                onChange={e => setSearchText(e.target.value)}
-            />
+            <Grid container direction="column" justify="center" align="center" spacing={2}>
+                <Grid item>
+                    <input
+                        className="search-input"
+                        placeholder="חפש מילואימניק"
+                        value={searchText}
+                        onChange={e => setSearchText(e.target.value)}
+                    />
+                </Grid>
+
+                <Grid item>
+                    <SearchResults searchText={searchText} />
+                </Grid>
+            </Grid>
         </div>
     )
 }
